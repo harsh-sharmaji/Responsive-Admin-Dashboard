a5ba7cf3c74747955749eebc111c4ec956787ed4
Harsh Sharma <133890014+harsh-sharmaji@users.noreply.github.com>
2024-07-02 18:33:11 +0530

    added

---
+++ script.js
@@ -0,0 +1,20 @@
+const sideMenu = document.querySelector('aside');
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const themeToggler = document.querySelector(".theme-toggler");
+//show bar
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = "block";
+})
+
+
+//close bar
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display = "none";
+})
+
+//change theme
+themeToggler.addEventListener("click", () => {
+    document.body.classList.toggle('dark-theme-variables');
+})
+
